69ce07e4e959bb906175f6df4c302a761cae5449
Misha <mishagrigoryan@gmail.com>
2023-05-18 23:05:46 -0400

    added inputs

---
+++ pyshorteners.py
@@ -0,0 +1,12 @@
+import pyshorteners
+
+import pyshorteners
+
+url = input("Enter the URL to shorten: ")
+
+shortener = pyshorteners.Shortener()
+
+short_url = shortener.tinyurl.short(url)
+
+print("Shortened URL:", short_url)
+
