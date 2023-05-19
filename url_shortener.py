ed618dbc41200aaee6ba5fa3993db0485346ab75
Misha <mishagrigoryan@gmail.com>
2023-05-18 23:11:52 -0400

    fixed file name (circual input)

--- url_shortener.py
+++ url_shortener.py
@@ -1,11 +1,8 @@
 import pyshorteners
 
-
 url = input("Enter the URL to shorten: ")
 
 shortener = pyshorteners.Shortener()
-
 short_url = shortener.tinyurl.short(url)
 
 print("Shortened URL:", short_url)
-
